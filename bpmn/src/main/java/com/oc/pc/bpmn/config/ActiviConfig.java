b0a4206d956fba994123f417db8bcdb7d6305e43
Camilo López <camilo.lopez@globant.com>
2021-02-26 11:06:13 -0300

    Database configuration

--- bpmn/src/main/java/com/oc/pc/bpmn/config/ActiviConfig.java
+++ bpmn/src/main/java/com/oc/pc/bpmn/config/ActiviConfig.java
@@ -1,6 +1,5 @@
 package com.oc.pc.bpmn.config;
 
 public class ActiviConfig {
-	
-            
+		
 }
---
+++ bpmn/src/main/java/com/oc/pc/bpmn/domain/PQR.java
@@ -0,0 +1,10 @@
+package com.oc.pc.bpmn.domain;
+
+import lombok.Data;
+
+@Data
+public class PQR {
+	private String trackingNumber;
+	private String fullName;
+	private String email;
+}
